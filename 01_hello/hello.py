8654754f14e8cc298014a23ac7a7ec717d6316e7
Chuck Conway <charles@cconway.com>
2020-08-18 08:38:32 -0700

    Competed chapter 1.

--- 01_hello/hello.py
+++ 01_hello/hello.py
@@ -1,2 +1,35 @@
+#!/usr/bin/env python3
+"""
+What is the purpose of this script...
+"""
+
+import argparse
+
+
+def get_args():
+    """Get the command-line arguments"""
+
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+        description='Say Hello')
+
+    parser.add_argument(
+        '-n',
+        '--name',
+        metavar="name",
+        default='World',
+        help='Name to greet')
+
+    return parser.parse_args()
+
+
+def main():
+    args = get_args()
+    print('Hello, ' + args.name + '!')
+
+
 # Say hello
-print("Hello, World!")
+# print("Hello, World!")
+
+if __name__ == '__main__':
+    main()
